6da73c0b1f44b810539d85cced92ecfa872b4a52
daniel kassampu tshimanga <102564408+dtshimanga14@users.noreply.github.com>
2022-11-10 20:11:08 -0600

    Dynamic programming bottom-up

--- src/main/java/org/example/p1.java
+++ src/main/java/org/example/p1.java
@@ -15,6 +15,46 @@ public class p1 {
         System.out.println(Singleton.getInstance());
         System.out.println(isValid("([{{])"));
     }
+
+    public static String numberToRoman(int number) {
+        String I[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String X[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String C[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String M[] = {"","M","MM","MMM"};
+
+        return M[number/1000] + C[(number%1000)/100] +
+                X[(number%100)/10] + I[(number)%10];
+    }
+    public static int romanToNumber(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        map.put('I',1); map.put('V',5); map.put('X',10);
+        map.put('C',100); map.put('L',50); map.put('M',1000);
+        int sum = 0; int sLen = s.length();
+
+        for(int i = 0; i < sLen-1; i++){
+            if(map.get(s.charAt(i)) > map.get(s.charAt(i+1))) {
+                sum += map.get(s.charAt(i));
+                System.out.println(sum);
+            }else sum -= map.get(s.charAt(i));
+        }
+
+        return sum+map.get(s.charAt(sLen-1));
+    }
+    public static int stickBottomUp(int sticks[], int stickLength) {
+        int coinsLen = sticks.length;
+        int res = Integer.MAX_VALUE;
+        Integer dp[] = new Integer[stickLength+1];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+
+        dp[0] = 0; //dp[1] = 1;
+        for(int i = 0; i <= stickLength; i++){
+            for(int j = 0; j < coinsLen; j++){
+                if(i-sticks[j] >= 0)
+                    dp[i] = Math.min(1+dp[i-sticks[j]],dp[i]);
+            }
+        }
+        return dp[stickLength] < 0 ? -1 : dp[stickLength];
+    }
     public static int numberOfRepeatingCharacter(String s) {
         Set<Character> set = new HashSet<>(); int sLen = s.length();
         int count = 0;
